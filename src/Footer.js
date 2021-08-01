95cc6a437abf033fb1d340e5e45da09ed193eb88
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-08-01 23:54:27 +0300

    working on adding the components

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+class Footer extends react.Component {
+
+  render() {
+    return(<footer></footer>)
+  }
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+class Main extends react.Component {
+
+  render() {
+    return(<main></main>);
+  }
+}
+
+export default Main;
